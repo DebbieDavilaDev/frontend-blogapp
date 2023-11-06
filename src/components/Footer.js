b55ff134ddba2ce90d24c51dcff3bcabad99aa35
Debbie Davila <debbie@debbiedavila.com>
2023-11-06 08:53:35 -0500

    frontend blogapp

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <small>Deborina Dávila 🦩 copyright © 2023 | all rights reserved</small>
+            </footer>
+        </>
+    )
+}
---
+++ src/pages/HomeC.js
@@ -0,0 +1,13 @@
+import Nav from '../components/Nav.js'
+import Footer from '../components/Footer.js'
+import CardContainer from '../components/CardContainer.js'
+
+export default function Home() {
+    return (
+        <>
+            <Nav />
+            <CardContainer />
+            <Footer />
+        </>
+    )
+}
